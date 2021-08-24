873003e16660f991713ebdc675c2f96142502257
Parth Manaktala <manaktala.parth@gmail.com>
2021-08-24 23:45:53 +0530

    Added HealthCheck

---
+++ src/main/java/com/pmanaktala/zippingservice/ZippingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.pmanaktala.zippingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZippingserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZippingserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pmanaktala/zippingservice/ZippingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pmanaktala.zippingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZippingserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
